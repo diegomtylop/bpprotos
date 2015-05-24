98ff11ea20094f8ec6cb85a5ecbc2ef8eeb4f98e
Diego Montoya <diego.mtylop@gmail.com>
2015-05-24 12:30:39 -0500

    Prototipo con navegación inicial
    Protiotipo creado con ionic como prueba de concepto y para analizar la
    navegación en la aplicación para la creación de itinerarios  y la
    adición de actividades.

---
+++ proto/www/js/app.js
@@ -0,0 +1,116 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+//Configura las rutas de navegación
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+  //Pantalla de creación de un nuevo itinerario
+  .state('app.create', {
+    url: "/create",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/create.html",
+          controller: 'AppCtrl'
+      }
+    }
+  })
+
+  .state('app.browse', {
+    url: "/browse",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/browse.html"
+      }
+    }
+  })
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+  
+  .state('app.begin', {
+    url: "/begin",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/selectmode.html"
+      }
+    }
+  })
+  
+  //Pantalla para listar las actividades
+  .state('app.activityList', {
+    url: "/activity",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/activityList.html",
+          controller: 'AppCtrl'
+      }
+    }
+  })
+  
+  //Pantalla para crear una actividad
+  .state('app.activityedit', {
+    url: "/activityedit",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/activityEdit.html",
+          controller: 'AppCtrl'
+      }
+    }
+  })
+
+  .state('app.single', {
+    url: "/playlists/:playlistId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlist.html",
+        controller: 'PlaylistCtrl'
+      }
+    }
+  })
+  
+  //Pantalla par visualizar el mapa
+  .state('app.mapa', {
+    url: "/mapa",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/mapa.html",
+        controller:"MapaController"
+      }
+    }
+  })
+  ;
+  // if none of the above states are matched, use this as the fallback
+  //$urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/app/begin');
+});
---
+++ proto/www/js/controllers.js
@@ -0,0 +1,116 @@
+angular.module('starter.controllers', [])
+
+
+//Controlador del itinerario y las actividades
+.controller('AppCtrl', function($scope, $ionicModal, $timeout,$state) {
+  // Form data for the login modal
+  $scope.loginData = {};
+    
+    $scope.itinerario = {
+        nombre:'Nombre itinerario',
+        fechaSalida:new Date(30,06,2015),
+        descripcion:'descripción ingresada desde el controlador',
+        presupuestDisponible:'100000'
+    };
+    
+    $scope.actividad = {
+        nombre:'Nueva actividad',
+        costo:'20000'
+    }
+    
+    $scope.dias = [
+        {
+            id:'1',
+            notas:'Notas de la actividad 1'
+        },
+        {
+            id:'2',
+            notas:'Notas de la actividad 2'
+        },
+        {
+            id:'3',
+            notas:'Notas de la actividad 3'
+        },
+        {
+            id:'4',
+            notas:'Notas de la actividad 4'
+        }
+    ]
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+    
+    // función que guarda la información general del itinerario
+  $scope.saveItinerario = function() {
+    console.log($scope.itinerario);
+      console.log('debe irse para otra parte');
+      //Redirige a la lista de actividades
+    $state.go('app.activityList');
+  };
+    
+    $scope.guardarActividad = function(){
+        console.log( $scope.actividad );
+        $state.go('app.activityList');
+    }
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+//Conrtolador de los mapas
+.controller('MapaController', function($scope) {
+
+    $scope.pais = "Colombia";
+    
+    //Función que obtiene la posición actual mediante el GPS
+    $scope.obtenerPosicionActual = function() {
+    if (navigator.geolocation) {
+        console.log('Tiene la posición');
+        navigator.geolocation.getCurrentPosition( function(position){
+            console.log('pos');
+            console.log(position.coords);
+            $scope.posicion = position.coords.latitude +" "+ position.coords.longitude;
+        });
+    } else {
+        $scope.posicion = "Geolocation is not supported by this browser.";
+    }
+}
+  
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
+
