f4ffb2749c3ca7b435e3a2101d2c4d03be41c26c
Diego Montoya <diego.mtylop@gmail.com>
2016-03-13 14:48:49 -0500

    nclusión del prototipo funcional

---
+++ funcional/www/js/controllers.js
@@ -0,0 +1,102 @@
+angular.module('app.controllers', [])
+     
+.controller('planInformaciNGeneralCtrl', function($scope) {
+
+})
+      
+.controller('planListaDeDAsCtrl', function($scope) {
+
+})
+   
+.controller('planListaDeDAsVistaActividadesCtrl', function($scope) {
+
+})
+   
+.controller('planListaDeDAsVistaLugaresCtrl', function($scope) {
+
+})
+   
+.controller('planAAdirDACtrl', function($scope) {
+
+})
+   
+.controller('planActividadesDACtrl', function($scope) {
+
+})
+   
+.controller('inicioCtrl', function($scope) {
+
+})
+   
+.controller('misViajesPlaneadosCtrl', function($scope) {
+
+})
+   
+.controller('misViajesSugeridosCtrl', function($scope) {
+
+})
+   
+.controller('misViajesRealizadosCtrl', function($scope) {
+
+})
+   
+.controller('actualNuevoGastoCtrl', function($scope) {
+
+})
+   
+.controller('actualActividadesParaHoyCtrl', function($scope) {
+
+})
+   
+.controller('actualActividadesParaMaAnaCtrl', function($scope) {
+
+})
+   
+.controller('actualResumenGastosCtrl', function($scope) {
+
+})
+      
+.controller('planEditarActividadCtrl', function($scope) {
+
+})
+   
+.controller('actualEditarActividadCtrl', function($scope) {
+
+})
+   
+.controller('actualDetallesDelViajeCtrl', function($scope) {
+
+})
+   
+.controller('planPresupuestoPlaneadoCtrl', function($scope) {
+
+})
+   
+.controller('gralMisActividadesCtrl', function($scope) {
+
+})
+   
+.controller('gralMisActividadesPBlicasCtrl', function($scope) {
+
+})
+   
+.controller('gralActividadesSugeridasCtrl', function($scope) {
+
+})
+   
+.controller('gralAAdirActividadCtrl', function($scope) {
+
+})
+   
+.controller('recordarViajeCtrl', function($scope) {
+
+})
+   
+.controller('miPerfLCtrl', function($scope) {
+
+})
+   
+.controller('introCtrl', function($scope) {
+
+})
+ 
